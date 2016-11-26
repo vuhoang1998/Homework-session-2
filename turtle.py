2eb61b7dd38396f3fbf45cbf2cc14b2ccaa63796
vuhoang1998 <rabac1998@gmail.com>
2016-11-26 17:57:26 +0700

    Create turtle.py

---
+++ turtle.py
@@ -0,0 +1,30 @@
+# Homework-session-2
+##from turtle import*
+##color("green","yellow")
+##begin_fill()
+##for i in range(4) :
+##    forward(100)
+##    lt(90)
+##end_fill()
+
+
+##from turtle import*
+##color("green","yellow")
+##begin_fill()
+##for i in range(3) :
+##    forward(100)
+##    lt(120)
+##end_fill()
+
+##from turtle import*
+##color("green","yellow")
+##begin_fill()
+##circle(100)
+##end_fill()
+
+
+from turtle import*
+color("green")
+for i in range(6) :
+    circle(100)
+    lt(60)
