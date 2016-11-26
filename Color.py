d133132aa15b718158ff23a11689f2a25976c519
vuhoang1998 <rabac1998@gmail.com>
2016-11-26 17:57:59 +0700

    Add files via upload

---
+++ Color.py
@@ -0,0 +1,9 @@
+color = ["Blue","Red","Black","Pink","Brown","Yellow"]
+print("My color list")
+print(color)
+print("color_list at index 3:",color[3])
+i = int(input("Enter a number from 0-5:"))
+print("Your color:",color[i])
+print(color)
+for i in range(len(color)) : 
+      print(color[i])
